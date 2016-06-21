9b2531ff08ca4ea3d6ac9bc9e85e63537a35467a
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-06-21 22:19:43 +0530

    registration login and landing page of pf repo

---
+++ register/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import RegistrationForm
+from django.contrib.auth.models import User
+# Create your views here.
+
+
+
+def register(request):
+    form = RegistrationForm(request.POST or None)
+    if form.is_valid():
+        if request.method == 'POST':
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = User(username=username, email=email, password=password)
+            user.set_password(password)
+            user.save()
+            print 'user registered'
+
+            user = authenticate(username=username, password=password)
+            context = {'user' : user}
+
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return render(request, 'dashboard.html', context)
+
+
+
+    return render(request, 'register.html', {form:form})
+
+
+def home(request):
+    user = request.user
+    return render(request, 'dashboard.html')
